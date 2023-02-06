82a3e25f04e40136b7b2abd85140be4df5103c02
oluwafemi <oluwafemi@DESKTOP-1TB3K83.localdomain>
2023-02-06 13:00:58 +0100

    add groovy script for jenkinsfile

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildImage(String imageName) {
+    echo 'Building the image using DockerFile'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    
+    sh 'cd frontend'
+    sh "docker build -t $imageName ."
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh "docker push $imageName"
+
+    }
+}
+
+def testdockerAgent(){
+    sh 'cd frontend'
+    sh 'npm install'
+}
